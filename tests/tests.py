6829398af84c07fe746d1688d7cad7c580d92b48
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-07-01 14:51:09 -0400

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,37 @@
-
-
+# user-statistician: Github action for generating a user stats card
+# 
+# Copyright (c) 2021 Vincent A Cicirello
+# https://www.cicirello.org/
+#
+# MIT License
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 
 import unittest
 
-# Your module imported assuming working directory
-# is root of repository. See workflow in repository
-# for example running of test cases.
-import entrypoint as ep
+import UserStatistician as UserStats
 
 class TestSomething(unittest.TestCase) :
 
     def test_sometestcase(self) :
-        # Unit test cases would go in these
-        # test methods to test the various
-        # Python functions, methods, etc.
-        pass
+        stats = UserStats.Statistician()
 
     def test_anothertestcase(self) :
         pass
