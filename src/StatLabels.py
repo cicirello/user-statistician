cfa91eb5df3625fc6650d6dca46e69f88e2076f4
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-07-06 13:40:14 -0400

    Update StatLabels.py

--- src/StatLabels.py
+++ src/StatLabels.py
@@ -63,7 +63,7 @@ statLabels = {
         "label" : "Archived"
         },
     
-    "commits:" : {
+    "commits" : {
         "icon" : '<path fill-rule="evenodd" d="M10.5 7.75a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm1.43.75a4.002 4.002 0 01-7.86 0H.75a.75.75 0 110-1.5h3.32a4.001 4.001 0 017.86 0h3.32a.75.75 0 110 1.5h-3.32z"/>',
         "label" : "Commits"
         },
