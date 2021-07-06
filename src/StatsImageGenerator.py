dda33a3c7a1ce356789fada7e3ae1136daf6900d
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-07-06 13:30:59 -0400

    Update StatsImageGenerator.py

--- src/StatsImageGenerator.py
+++ src/StatsImageGenerator.py
@@ -25,6 +25,7 @@
 # SOFTWARE.
 #
 
+from StatLabels import statLabels
 
 class StatsImageGenerator :
     """Generates an svg image from the collected stats."""
@@ -33,6 +34,20 @@ class StatsImageGenerator :
     backgroundTemplate = '<rect x="1" y="1" stroke-width="2" rx="5" width="348" height="{0}" stroke="{1}" fill="{2}" />'
     fontGroup = '<g font-weight="600" font-family="Verdana,Geneva,DejaVu Sans,sans-serif">'
     titleTemplate = '<text x="15" y="35" font-size="16px" fill="{1}">{0}</text>'
+    groupHeaderTemplate = '<g transform="translate(0, {0})" font-size="14px" fill="{1}">'
+    tableEntryTemplate = """<g transform="translate(15, {0})">
+<svg viewBox="0 0 16 16" width="16" height="16" fill="{1}">
+{2}
+</svg>
+<text x="25" y="12.5">{3}:</text>
+<text x="160" y="12.5">{4}</text>
+<text x="200" y="12.5">{5}</text>
+</g>"""
+    tableHeaderTemplate = """<g transform="translate(15, 0)">
+<text x="0" y="12.5">{1}:</text>
+<text x="160" y="12.5">{2}</text>
+<text x="200" y="12.5">{3}</text>
+</g>"""
     
     __slots__ = [
         '_stats',
@@ -57,17 +72,46 @@ class StatsImageGenerator :
             StatsImageGenerator.fontGroup
             ]
 
-    def generateImage(self, includeTitle, customTitle) :
+    def generateImage(self, includeTitle, customTitle, exclude) :
         """Generates and returns the image.
 
         Keyword arguments:
         includeTitle - If True inserts a title.
         customTitle - If not None, this is used as the title, otherwise title is formed
             from user's name.
+        exclude - Set of keys to exclude.
         """
         self.insertTitle(includeTitle, customTitle)
+        self.insertGroup(
+            self._stats._repo,
+            ["Repositories", "Non-Forks", "With Forks"],
+            self.filterKeys(
+                self._stats._repo,
+                exclude,
+                ["public", "starredBy", "forkedBy", "watchedBy", "archived"]
+                )
+            )
+        self.insertGroup(
+            self._stats._contrib,
+            ["Contributions", "Past Year", "All"],
+            self.filterKeys(
+                self._stats._contrib,
+                exclude,
+                ["commits", "issues", "prs", "reviews", "contribTo", "private"]
+                )
+            )
         self.finalizeImageData()
         return "\n".join(self._rows)
+
+    def filterKeys(self, data, exclude, keys) :
+        """Returns a list of the keys that have non-zero data and which are not excluded.
+
+        Keyword arguments:
+        data - The data (either contrib or repo data)
+        exclude - A set of keys to exclude
+        keys - The list of keys relevant for the table.
+        """
+        return [ k for k in keys if k not in exclude and k in data and (data[k][0] > 0 or data[k][1] > 0) ]
 
     def insertTitle(self, includeTitle, customTitle) :
         """Generates, formats, and inserts title.
@@ -85,10 +129,38 @@ class StatsImageGenerator :
             self._rows.append(StatsImageGenerator.titleTemplate.format(title, self._colors["title"]))
             self._height += 35
 
-    def insertGroup(self, data, exclude, headerRow, keys) :
-        pass
-        
+    def insertGroup(self, data, headerRow, keys) :
+        """Generates the portion of the image for a group
+        (i.e., the repositories section or the contributions section).
+        If there are no keys with data, then this does nothing (excludes
+        all in group).
 
+        Keyword arguments:
+        data - A dictionary with the data.
+        headerRow - A list with the header row text.
+        keys - A list of keys in the order they should appear.
+        """
+        if len(keys) > 0 :
+            self._height += 20
+            self._rows.append(StatsImageGenerator.groupHeaderTemplate.format(self._height, self._colors["text"]))
+            self._rows.append(StatsImageGenerator.tableHeaderTemplate.format(
+                headerRow[0],
+                headerRow[1],
+                headerRow[2])
+            offset = 25
+            for k in keys :
+                self._rows.append(StatsImageGenerator.tableEntryTemplate.format(
+                    str(offset),
+                    self._colors["icons"],
+                    statLabels[k]["icon"],
+                    statLabels[k]["label"],
+                    data[k][0],
+                    data[k][1]
+                    ))
+                offset += 25
+            self._rows.append("</g>")
+            self._height += offset
+        
     def finalizeImageData(self) :
         """Inserts the height into the svg opening tag and the rect for the background.
         Also inserts the border and background colors into the rect for the background.
