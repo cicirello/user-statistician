5d4128c253ac0a50686e9b564bace68ae0bd268e
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-07-06 15:33:51 -0400

    Update StatsImageGenerator.py

--- src/StatsImageGenerator.py
+++ src/StatsImageGenerator.py
@@ -166,8 +166,8 @@ class StatsImageGenerator :
                     self._colors["icons"],
                     statLabels[k]["icon"],
                     statLabels[k]["label"],
-                    self.format(data[k][0]),
-                    self.format(data[k][1]) if len(data[k]) > 1 else ""
+                    self.formatCount(data[k][0]),
+                    self.formatCount(data[k][1]) if len(data[k]) > 1 else ""
                     ))
                 offset += 25
             self._rows.append("</g>")
